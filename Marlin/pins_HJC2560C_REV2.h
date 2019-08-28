da7b72ab58a8a0c04d602fe401ee86195fcd2180
Mark Newnham <mark@newnhams.com>
2019-08-27 18:51:44 -0600

    Enable fan_pin to support dual fans
    Supports 2 fans, 1 static for hotend cooler, 1 variable for parts cooling

--- Marlin/pins_HJC2560C_REV2.h
+++ Marlin/pins_HJC2560C_REV2.h
@@ -97,9 +97,9 @@
 #define HEATER_1_PIN        3
 #define HEATER_BED_PIN      4
 
-//#ifndef FAN_PIN
-//  #define FAN_PIN           7  //默认不使用PWM_FAN冷却喷嘴，如果需要，则取消注释
-//#endif
+#ifndef FAN_PIN
+  #define FAN_PIN           7  //Dual Fan Option, needs hardware mod
+#endif
 
 //
 // Misc. Functions
